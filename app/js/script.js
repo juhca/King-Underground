182e2266a9596b58fc08139b87de85317bbdec96
Tomas <tomas.stefe@gmail.com>
2016-11-15 12:32:00 +0100

    Dodajanje osnovnih textur

--- app/js/script.js
+++ app/js/script.js
@@ -23,6 +23,20 @@ function onload() {
 
 function initScene() {
     scene = new BABYLON.Scene(engine);
+        /*
+        * FOG
+        * */
+    //scene.fogMode = BABYLON.Scene.FOGMODE_EXP;
+    //scene.fogDensity = 0.1;
+
+        /*
+        * Background music
+        * */
+
+    //var music = new BABYLON.Sound("Music", "01. BT-7274.mp3", scene, null, {
+    //   loop: true,
+    //   autoplay: true
+    //});
 
     var camera = new BABYLON.FreeCamera('camera', new BABYLON.Vector3(0, 50, -50), scene);
     camera.setTarget(BABYLON.Vector3.Zero());
@@ -58,8 +72,10 @@ function initScene() {
 
 
     var groundMaterial = new BABYLON.StandardMaterial('ground', scene);
-    groundMaterial.diffuseColor = new BABYLON.Color3.Gray();
+	groundMaterial.diffuseTexture = new BABYLON.Texture("floor.jpg",scene);
     groundMaterial.specularColor = new BABYLON.Color3.Black();
+    groundMaterial.diffuseTexture.uScale = 5.0;
+    groundMaterial.diffuseTexture.vScale = 5.0;
 
     var ground = BABYLON.MeshBuilder.CreateBox('ground', {
         size: roomLength,
@@ -70,7 +86,11 @@ function initScene() {
 
 
     var wallMaterial = new BABYLON.StandardMaterial('wall', scene);
-    wallMaterial.diffuseColor = new BABYLON.Color3.Gray();
+    wallMaterial.diffuseTexture = new BABYLON.Texture("wall.jpg", scene);
+    wallMaterial.specularColor = new BABYLON.Color3.Black();
+    wallMaterial.diffuseTexture.uScale = 1.5;
+    wallMaterial.diffuseTexture.vScale = 4.0;
+    //wallMaterial.diffuseColor = new BABYLON.Color3.Gray();
 
     // +x
     var wall = BABYLON.MeshBuilder.CreateBox('wall1', {
@@ -127,13 +147,16 @@ function initScene() {
     wall8.rotation.y += Math.PI / 4;
 
     var ceilMat = new BABYLON.StandardMaterial('ceilMat', scene);
-    ceilMat.diffuseColor = new BABYLON.Color3.Blue();
+    ceilMat.diffuseTexture = new BABYLON.Texture('roof.jpg', scene);
+    ceilMat.specularColor = new BABYLON.Color3.Yellow();
+    ceilMat.diffuseTexture.uScale = 1.0;
+    ceilMat.diffuseTexture.vScale = 1.0;
 
     var sphereDiameter = roomLength - 2 * wallWidth;
     var ceilSphere = BABYLON.MeshBuilder.CreateSphere('ceilSphere', {
         diameter: sphereDiameter,
         slice: 0.5,
-        sideOrientation: BABYLON.Mesh.DOUBLESIDE
+        sideOrientation: BABYLON.Mesh.BACKSIDE
     }, scene);
     ceilSphere.position.y += wallHeight;
     ceilSphere.material = ceilMat;
@@ -156,7 +179,12 @@ function initScene() {
     var stepNum = 3;
 
     var platformMat = new BABYLON.StandardMaterial('platform', scene);
-    platformMat.diffuseColor = new BABYLON.Color3.Blue();
+    platformMat.diffuseTexture = new BABYLON.Texture("floor1.jpg", scene);
+    platformMat.specularColor = new BABYLON.Color3.Black();
+    platformMat.diffuseTexture.uScale = 3.0;
+    platformMat.diffuseTexture.vScale = 3.0;
+    //platformMat.diffuseColor = new BABYLON.Color3.Blue();
+
 
     var platform = false;
 
@@ -186,7 +214,11 @@ function initScene() {
     var pillarLocation = ((stepSize - 4 * stepNum * stepHeight) / 2) * 7 / 8;
 
     var pillarMat = new BABYLON.StandardMaterial('pillar', scene);
-    pillarMat.diffuseColor = new BABYLON.Color3.Gray();
+    pillarMat.diffuseTexture = new BABYLON.Texture("pillar.jpg", scene);
+    pillarMat.specularColor = new BABYLON.Color3.Black();
+    pillarMat.diffuseTexture.uScale = 1.5;
+    pillarMat.diffuseTexture.vScale = 2.0;
+    //pillarMat.diffuseColor = new BABYLON.Color3.Gray();
 
     var pillar = BABYLON.MeshBuilder.CreateCylinder('pillar1', {
         height: pillarHeight,
